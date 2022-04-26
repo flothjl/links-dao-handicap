b19c650307216193d150a553f6a496e52689cb1f
Josh Floth <thadiusbacon@gmail.com>
2022-04-25 19:58:20 -0500

    linting

--- contracts/LinksHandicap.sol
+++ contracts/LinksHandicap.sol
@@ -5,10 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-
-
-contract LinksHandicap is Ownable{
-
+contract LinksHandicap is Ownable {
     // Emitted when the stored value changes
     event ScoreAdded(
         address indexed who,
@@ -23,49 +20,54 @@ contract LinksHandicap is Ownable{
     }
 
     mapping(address => Score[]) scores;
-    mapping (address => uint) ownerScoreCount;
-
+    mapping(address => uint256) ownerScoreCount;
 
     IERC721 linksDao = IERC721(0x696115768BBEf67Be8bd408d760332A7EfbEE92D);
 
-    function setLinksDaoContract(address _address) public onlyOwner{
+    function setLinksDaoContract(address _address) public onlyOwner {
         linksDao = IERC721(_address);
     }
-    
-    modifier onlyLinksOwner {
+
+    modifier onlyLinksOwner() {
         require(linksDao.balanceOf(msg.sender) > 0, "Unauthorized");
         _;
     }
 
-    function addScore(int8 value, uint256 scoretime, string memory courseName) public onlyLinksOwner{
-        
-        scores[msg.sender].push(Score(
-            scoretime,
-            value,
-            courseName
-        ));
+    function addScore(
+        int8 value,
+        uint256 scoretime,
+        string memory courseName
+    ) public onlyLinksOwner {
+        scores[msg.sender].push(Score(scoretime, value, courseName));
 
         ownerScoreCount[msg.sender]++;
 
-        emit ScoreAdded(
-            msg.sender,
-            value,
-            courseName
-        );
+        emit ScoreAdded(msg.sender, value, courseName);
     }
 
     function addScoreBatch(Score[] memory _scores) public {
-        for (uint i=0; i<_scores.length; i++) {
-            addScore(_scores[i].score, _scores[i].scoretime, _scores[i].courseName);
+        for (uint256 i = 0; i < _scores.length; i++) {
+            addScore(
+                _scores[i].score,
+                _scores[i].scoretime,
+                _scores[i].courseName
+            );
         }
     }
 
-    function getScoresByOwner(address _address) public view returns (Score[] memory) {
+    function getScoresByOwner(address _address)
+        public
+        view
+        returns (Score[] memory)
+    {
         return scores[_address];
     }
 
-    function getScoreCountByOwner(address _address) public view returns (uint) {
+    function getScoreCountByOwner(address _address)
+        public
+        view
+        returns (uint256)
+    {
         return ownerScoreCount[_address];
     }
-
 }
